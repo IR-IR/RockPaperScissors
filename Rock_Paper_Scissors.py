6f0c66af5a306d3d3235804a4e802e669589fa47
Iftiar Rana <67529050+IR-IR@users.noreply.github.com>
2020-06-28 16:38:57 -0400

    Add files via upload

---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,37 @@
+Human_Score = 0 
+AI_Score = 0 
+Ties = 0 
+import random
+from random import randint 
+Options = ["rock","paper", "scissors" ]
+Computer_Choice = Options[randint(0,2)]
+x = "go" 
+
+while x == "go": 
+    Player_Choice = input("Please Select Your Choice of Weapon:")
+    
+    if Player_Choice == Computer_Choice:
+        print("Both of you have chosen the same weapon. It's a tie.")
+        Ties += 1
+   
+    elif Player_Choice == "rock" and Computer_Choice == "paper":
+        print("Paper covers rock. Computer wins.")
+        AI_Score += 1
+    elif Player_Choice == "paper" and Computer_Choice == "rock":
+        print("Paper covers rock. You win.")
+        Human_Score +=1
+    elif Player_Choice == "scissors" and Computer_Choice == "paper":
+        print("Scissors cut paper. Computer wins.")
+        AI_Score += 1
+    elif Player_Choice == "paper" and Computer_Choice == "scissors":
+        print("Scissors cut paper. You win.")
+        Human_Score +=1
+    elif Player_Choice == "scissors" and Computer_Choice == "rock":
+        print ("Rock crushes scissors. Computer wins.")
+        AI_Score +=1
+    elif Player_Choice == "rock" and Computer_Choice == "scissors":
+        print ("Rock crushes scissors. You win.")
+        Human_Score += 1
+    
+    print("You have", Human_Score, "wins. The AI has", AI_Score, "wins. Y'all drew", Ties, "times.")
+    
